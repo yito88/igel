29e934ebbf7b2e206160db3de10f933a9dc6796d
yito88 <yuji@phact-columba.com>
2023-08-27 17:30:06 +0200

    WIP: wal

--- src/igel/wal.clj
+++ src/igel/wal.clj
@@ -5,13 +5,22 @@
             [igel.store :as store]))
 
 (defn spawn-wal-writer
-  [wal-path recv-chan signal-chan]
-  (let [file-stream (FileOutputStream. wal-path)]
+  [wal-path recv-chan config]
+  (let [file-stream (FileOutputStream. wal-path)
+        sync-window (or (:sync-window-time config) 200)]
     (with-open [out-stream (BufferedOutputStream. file-stream 4096)]
-      (async/go-loop []
-        (io/append-wal! out-stream (async/<! recv-chan))
-          ;; TODO: wait for the minimum period instead of sync after every write
-          ;; fsync
-        (-> file-stream .getChannel (.force true))
-        (async/>! signal-chan :sync)
-        (recur)))))
+      (async/go-loop [prev-sync-time (System/currentTimeMillis)
+                      comp-channels (transient #{})]
+        (let [[k d comp-chan] (async/<! recv-chan)]
+          (io/append-wal! out-stream [k d])
+          (conj! comp-channels comp-chan)
+          (recur
+           (if (< (+ prev-sync-time sync-window) (System/currentTimeMillis))
+             (do
+               (-> file-stream .getChannel (.force true))
+               (doseq [comp-chan comp-channels]
+                 (async/>! comp-chan :done))
+               (reset! comp-channels (transient #{}))
+               (System/currentTimeMillis))
+             prev-sync-time)
+           comp-channels))))))
